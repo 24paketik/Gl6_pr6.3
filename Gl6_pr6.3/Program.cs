26415b16d3bb891fea2755b644590f10a40d6d8b
24paketik <deniska-smirnov-05@mail.ru>
2022-04-29 13:17:21 +0300

    create

---
+++ Gl6_pr6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Gl6_pr6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank_Client client = new Bank_Client(
+            "Ivanov", 1234567, 2000000, 2015, 1, 9);
+            client.put(50000); client.get(20000);
+            client.Person_Display();
+            client.percent();
+            // в конце года к вкладу добавляется бонус
+            client.put(client.bonus());
+            Console.WriteLine
+            (" сумма вклада: " + client.Summa_bank);
+            //===================================
+            Shop_Client client1 = new Shop_Client();
+            client1.New_Buy(); // первая покупка
+            client1.Summa_buy -= client1.bonus();
+            Console.WriteLine
+            ("к оплате: " + client1.Summa_buy);
+            client1.New_Buy(); // вторая покупка
+            client1.Summa_buy -= client1.bonus();
+            Console.WriteLine("к оплате: " + client1.Summa_buy);
+        }
+    }
+}
